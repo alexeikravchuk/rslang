48d0b94cb8f7a2750db2e9e39f73190843722a22
DzianisBogdan <thespirit3000@gmail.com>
2020-06-16 19:04:11 +0300

    refactor: action comments

--- src/store/actions/appStateAction.js
+++ src/store/actions/appStateAction.js
@@ -1,12 +1,14 @@
 const ADD_DAY_COUNTER = 'ADD_DAY_COUNTER';
 const RESET_DAY_COUNTER = 'RESET_DAY_COUNTER';
 
+// INCREMENT day word counter
 export const addDay = () => {
   return ({
     type: ADD_DAY_COUNTER,
   })
 }
 
+//RESET every day counter
 export const reset = () => {
   return ({
     type: RESET_DAY_COUNTER,
