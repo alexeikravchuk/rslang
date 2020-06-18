d91a9df6d647b3364e0d2bb60f226426700eab72
DzianisBogdan <thespirit3000@gmail.com>
2020-06-18 23:15:37 +0300

    feat: base game components

---
+++ src/pages/Savannah/Savannah.js
@@ -0,0 +1,15 @@
+import React  from "react";
+// import ReactDom from "react-dom";
+
+import StartGame from './components/StartGame/StartGame'
+import './Savannah.scss'
+
+function Savannah () {
+  return (
+    <div>
+      <StartGame />
+    </div>
+  )
+}
+
+export default Savannah;
---
+++ src/pages/Savannah/components/StartGame/StartGame.js
@@ -0,0 +1,12 @@
+import React  from "react";
+import Button from '@material-ui/core/Button'
+function StartGame() {
+  return (
+    <div className={'container'}>
+      <Button variant={'contained'} color={'primary'}>Game must go on...</Button>
+      <p> Click on button please...</p>
+    </div>
+  )
+}
+
+export default StartGame
