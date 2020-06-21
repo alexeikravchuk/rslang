48d63b9fce1fef22211eddb7a9121f8b1784d28d
Vladislav Shevel <vladislav.shevel@gmail.com>
2020-06-21 21:06:32 +0300

    fix: deleted HomePage.scss and used backgroundImage field in makeStyles.homePage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import './HomePage.scss';
+import Img from './HomePageImg/homepagebg.png';
 
 const useStyles = makeStyles({
   homePage: {
+    backgroundImage: `url(${Img})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
     height: '100vh',
     display: 'flex',
     flexDirection: 'column',
@@ -20,8 +23,8 @@ function HomePage() {
     console.log('Play Button Clicked');
   }
   return (
-    <Container className={classes.homePage}>
-      <Button variant='contained' color='primary' size='large' onClick={ handlePLayClick }>
+    <Container className={classes.homePage }>
+      <Button variant='contained' color='primary' size='large' onClick={handlePLayClick}>
         Let's Play RsLang
       </Button>
     </Container>
