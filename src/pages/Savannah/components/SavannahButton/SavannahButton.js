5a65df630c6d883fb8e4b90fba0cf4d41e8baaef
DzianisBogdan <thespirit3000@gmail.com>
2020-06-21 15:30:17 +0300

    feat: button component

---
+++ src/pages/Savannah/components/SavannahButton/SavannahButton.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme=> ({
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(106, 35, 72, .3)',
+    color: 'wheat',
+    height: 48,
+    padding: '0 30px',
+  },
+}));
+
+export default (props) => {
+  const classes = useStyles();
+  return (
+    <Button variant={'outlined'} className={classes.button} onClick={props.onClick}>
+      {props.title}
+    </Button>
+    )
+}
