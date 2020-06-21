80356d037c1ad5f794ee296bfcb60a86079d278a
DzianisBogdan <thespirit3000@gmail.com>
2020-06-21 19:10:32 +0300

    feat: MainGame component

--- src/pages/Savannah/components/MainGame/MainGame.js
+++ src/pages/Savannah/components/MainGame/MainGame.js
@@ -1,19 +1,9 @@
-import React, {Component} from "react";
-import {withStyles} from "@material-ui/core/styles";
-import {connect} from "react-redux";
-import Container from "@material-ui/core/Container";
-import GameToolbar from "../GameToolbar/Toolbar";
+import React from "react";
+import Box from "@material-ui/core/Box";
 import Background from "../background/Background";
-import {Statistics} from "../Statistics";
-import {
-  gameEnding,
-  loadWords,
-  loadWordsSuccess,
-} from "../../../../store/actions/savannahAction";
-import {getWords} from "../../utils/wordRequest";
-import {StartGame} from "../StartGame";
-import GameButtonGroup from "../ButtonGroup/ButtonGroup";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import {withStyles} from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const styles ={
   root: {
@@ -21,65 +11,20 @@ const styles ={
     zIndex: 10,
     display: 'flex',
     flexDirection: 'column',
-    alignContent: 'center',
-    justifyContent: 'center'
+    alignContent: 'center'
   },
-  title: {
-    flexGrow: 1,
-    textAlign: 'center',
-    color: "#662246",
-  },
-  loader: {
-    position: 'relative',
-    zIndex: 10,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height:'80vh',
-    width: '100%'
-  }
 }
 
-class MainGame extends Component{
-  loader(classes) {
-    return (
-      <Container className={classes}>
-        <CircularProgress />
+function MainGame(props) {
+  const {classes} = props;
+  return (
+    <div>
+      <Container className={classes.root}>
+        
       </Container>
-    )
-  }
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <GameToolbar title={''} onClose={this.props.onClose}/>
-        {this.props.loading && this.loader(classes.loader)}
-        <Container className={classes.root}>
-          {this.props.gameStarted === false && <StartGame />}
-          {this.props.gameEnd && <Statistics />}
-          {(this.props.gameStarted && !this.props.gameEnd) && <GameButtonGroup
-            fetch={this.props.fetchWords}
-            visible={true}
-          />}
-        </Container>
-        <Background />
-      </div>
-    )
-  }
+      <Background />
+    </div>
+  )
 }
 
-function mapStateToProps(store){
-  const { savannahReducer } = store
-  return { ...savannahReducer }
-}
-
-const mapDispatchToProps = dispatch => ({
-  fetchWords: (page, category) => {
-    dispatch(loadWords())
-    getWords(page, category)
-      .then(json => dispatch(loadWordsSuccess(json)))
-  },
-  onClose: ()=> dispatch(gameEnding())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(MainGame));
+export default withStyles(styles)(MainGame);
