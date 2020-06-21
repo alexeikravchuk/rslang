5478d64f312065648381fb9e7ef7ee5dd26322a4
DzianisBogdan <thespirit3000@gmail.com>
2020-06-21 23:39:31 +0300

    feat: savannah redux store

--- src/store/actions/savannahAction.js
+++ src/store/actions/savannahAction.js
@@ -1,88 +1,27 @@
 const ADD_NEW_WORDS = 'ADD_NEW_WORDS'
 const ADD_LEARNED_WORDS = 'ADD_LEARNED_WORDS'
 const ADD_MISSED_WORDS = 'ADD_MISSED_WORDS'
-const LOAD_WORDS = 'LOAD_WORDS'
-const LOAD_WORDS_SUCCESS = 'LOAD_WORDS_SUCCESS'
-const GAME_STARTING = 'GAME_STARTING'
-const LIFE_DECREASE = 'LIFE_DECREASE'
-const GAME_END = 'GAME_END'
-const RESET_GAME = 'RESET_GAME'
-const DIFFICULTY_CHANGE = 'DIFFICULTY_CHANGE'
-const REMOVE_WORD = 'REMOVE_WORD'
 
-const loadWords = () => {
+const addWords = () => {
   return (
     {
-      type: LOAD_WORDS,
+      type: ADD_NEW_WORDS,
     }
   )
 }
 
-const gameStarting = () => {
-  return (
-    {
-      type: GAME_STARTING,
-    }
-  )
-}
-
-const gameReset = () => {
-  return {
-    type: RESET_GAME,
-  }
-}
-
-const gameEnding = () => {
-  return {
-    type: GAME_END,
-  }
-}
-
-const loadWordsSuccess = (words) => {
-  return (
-    {
-      type: LOAD_WORDS_SUCCESS,
-      words
-    }
-  )
-}
-
-const difficultyChange = (difficulty) => {
-  return(
-    {
-      type: DIFFICULTY_CHANGE,
-      difficulty
-    }
-  )
-}
-
-const lifeDecrease = () => {
-  return {type: LIFE_DECREASE}
-}
-
-const addLearnedWords = (word) => {
+const addLearnedWords = () => {
   return (
     {
       type: ADD_LEARNED_WORDS,
-      word: word
     }
   )
 }
 
-const addMissedWords = (word) => {
+const addMissedWords = () => {
   return (
     {
       type: ADD_MISSED_WORDS,
-      word
-    }
-  )
-}
-
-const removeWord = (word) => {
-  return (
-    {
-      type: REMOVE_WORD,
-      word
     }
   )
 }
@@ -90,21 +29,6 @@ const removeWord = (word) => {
 export { ADD_NEW_WORDS,
   ADD_LEARNED_WORDS,
   ADD_MISSED_WORDS,
-  LOAD_WORDS,
-  LOAD_WORDS_SUCCESS,
-  GAME_STARTING,
-  GAME_END,
-  LIFE_DECREASE,
-  RESET_GAME,
-  DIFFICULTY_CHANGE,
-  REMOVE_WORD,
+  addWords,
   addLearnedWords,
-  addMissedWords,
-  loadWords,
-  loadWordsSuccess,
-  gameStarting,
-  gameEnding,
-  lifeDecrease,
-  gameReset,
-  difficultyChange,
-  removeWord}
+  addMissedWords}
