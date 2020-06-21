fd1037c4644c5679cefbbf544aaa9bf746f9e8e9
DzianisBogdan <thespirit3000@gmail.com>
2020-06-21 15:30:17 +0300

    feat: button component

--- src/pages/Savannah/components/SavannahButton/SavannahButton.js
+++ src/pages/Savannah/components/SavannahButton/SavannahButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import {makeStyles} from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme)=> ({
+const useStyles = makeStyles(theme=> ({
   button: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     border: 0,
@@ -10,16 +10,15 @@ const useStyles = makeStyles((theme)=> ({
     boxShadow: '0 3px 5px 2px rgba(106, 35, 72, .3)',
     color: 'wheat',
     height: 48,
-    width: 200,
-    margin: theme.spacing(1),
+    padding: '0 30px',
   },
 }));
 
 export default (props) => {
   const classes = useStyles();
   return (
-      <Button key={props.key} variant={'outlined'} className={classes.button} onClick={props.onClick}>
-        {props.title}
-      </Button>
+    <Button variant={'outlined'} className={classes.button} onClick={props.onClick}>
+      {props.title}
+    </Button>
     )
 }
