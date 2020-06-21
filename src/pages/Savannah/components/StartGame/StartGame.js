cd65ae3c978795c269543ae9ae48f200b6736b1e
DzianisBogdan <thespirit3000@gmail.com>
2020-06-21 23:36:51 +0300

    refactor: toolbar insert

--- src/pages/Savannah/components/StartGame/StartGame.js
+++ src/pages/Savannah/components/StartGame/StartGame.js
@@ -1,22 +1,13 @@
 import React  from "react";
 import { withStyles } from '@material-ui/core/styles';
-import Toolbar from "@material-ui/core/Toolbar";
 import Dialog from "@material-ui/core/Dialog";
 import Typography from "@material-ui/core/Typography";
 import Background from "../background/Background";
-
-import {CloseButton} from "../closeButton";
+import {GameToolbar} from "../GameToolbar";
 import {SavannahButton} from "../SavannahButton";
+import Container from "@material-ui/core/Container";
 
 const styles = {
-  title: {
-    flexGrow: 1,
-  },
-  toolBar:{
-    position: "relative",
-    textAlign: "center",
-    color: "#662246"
-  },
   contentDialog: {
     zIndex: 10,
     height: '100vh',
@@ -28,15 +19,6 @@ const styles = {
   contentText:{
     color: 'wheat',
     marginBottom: "1rem"
-  },
-  button: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(106, 35, 72, .3)',
-    color: 'wheat',
-    height: 48,
-    padding: '0 30px',
   },
 };
 
@@ -50,21 +32,15 @@ function StartGame(props) {
       <Dialog
         fullScreen
         open={open}
-        onClose={handleClose}
-      >
+        onClose={handleClose}>
+          <GameToolbar title={'Savannah game'} />
           <Background />
-          <Toolbar className={classes.toolBar}>
-              <Typography variant={'h4'} className={classes.title}>
-               Savannah Game
-              </Typography>
-              <CloseButton />
-          </Toolbar>
-          <div className={classes.contentDialog}>
-           <Typography variant={'h6'} className={classes.contentText}>
-             Game must begin...
-           </Typography>
+          <Container className={classes.contentDialog}>
+            <Typography variant={'h6'} className={classes.contentText}>
+              Game must begin...
+            </Typography>
            <SavannahButton title={'lets begin'} onClick={handleClose}/>
-         </div>
+         </Container>
        </Dialog>
 
   )
