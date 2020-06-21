cbdff8ddf7b919b8429234d01f3fb58c2b04666d
DzianisBogdan <thespirit3000@gmail.com>
2020-06-21 23:39:53 +0300

    refactor game structure

--- src/pages/Savannah/Savannah.js
+++ src/pages/Savannah/Savannah.js
@@ -1,13 +1,16 @@
 import React  from "react";
-// import ReactDom from "react-dom";
-
-import StartGame from './components/StartGame/StartGame'
+import {MainGame} from './components/MainGame'
+import { StartGame } from './components/StartGame'
+import { Statistics } from "./components/Statistics";
 import './Savannah.scss'
 
 function Savannah () {
   return (
     <div>
       <StartGame />
+      <MainGame />
+      <Statistics />
+
     </div>
   )
 }
