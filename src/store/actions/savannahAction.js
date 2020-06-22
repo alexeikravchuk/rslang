1eada868634c6d2053994c09dd148522cdb6b81c
DzianisBogdan <thespirit3000@gmail.com>
2020-06-23 00:51:05 +0300

    feat: fetch words to store

--- src/store/actions/savannahAction.js
+++ src/store/actions/savannahAction.js
@@ -1,6 +1,25 @@
 const ADD_NEW_WORDS = 'ADD_NEW_WORDS'
 const ADD_LEARNED_WORDS = 'ADD_LEARNED_WORDS'
 const ADD_MISSED_WORDS = 'ADD_MISSED_WORDS'
+const LOAD_WORDS = 'LOAD_WORDS'
+const LOAD_WORDS_SUCCESS = 'LOAD_WORDS_SUCCESS'
+
+const loadWords = () => {
+  return (
+    {
+      type: LOAD_WORDS,
+    }
+  )
+}
+
+const loadWordsSuccess = (words) => {
+  return (
+    {
+      type: LOAD_WORDS_SUCCESS,
+      words
+    }
+  )
+}
 
 const addWords = () => {
   return (
@@ -29,6 +48,10 @@ const addMissedWords = () => {
 export { ADD_NEW_WORDS,
   ADD_LEARNED_WORDS,
   ADD_MISSED_WORDS,
+  LOAD_WORDS,
+  LOAD_WORDS_SUCCESS,
   addWords,
   addLearnedWords,
-  addMissedWords}
+  addMissedWords,
+  loadWords,
+  loadWordsSuccess}
