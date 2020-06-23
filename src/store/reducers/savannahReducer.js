9759265b9823591c2cd25d0f1bc5573bede7629a
DzianisBogdan <thespirit3000@gmail.com>
2020-06-23 23:07:03 +0300

    feat: life decrease, game started flag

--- src/store/reducers/savannahReducer.js
+++ src/store/reducers/savannahReducer.js
@@ -1,15 +1,22 @@
-import { ADD_NEW_WORDS,
+import {
+  ADD_NEW_WORDS,
   ADD_LEARNED_WORDS,
   ADD_MISSED_WORDS,
   LOAD_WORDS,
-  LOAD_WORDS_SUCCESS } from '../actions/savannahAction';
+  LOAD_WORDS_SUCCESS,
+  GAME_STARTING,
+  LIFE_DECREASE
+} from '../actions/savannahAction';
 
 const defaultState = {
+  gameStarted: false,
   loading: false,
   error: null,
+  lifeCounter: 5,
   newWords: [],
   learnedWords: [],
   missedWords: [],
+
   currentWord:{
     word: '',
     translate: '',
@@ -18,11 +25,18 @@ const defaultState = {
 
 const savannahReducer = ( state = defaultState, action) => {
   switch (action.type) {
+    case GAME_STARTING: {
+      return {...state, gameStarted: true}
+    }
     case LOAD_WORDS: {
       return { ...state, loading: true, error: null}
     }
     case LOAD_WORDS_SUCCESS: {
       return { ...state, newWords: action.words , loading: false, error: null}
+    }
+    case LIFE_DECREASE: {
+      return { ...state,
+        lifeCounter: (state.lifeCounter === 0) ? 0 : state.lifeCounter-1}
     }
     case ADD_NEW_WORDS: {
       return {...state};
