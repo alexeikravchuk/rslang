0b2a6359a32472164793ec8542d4920a6f6a0aed
DzianisBogdan <thespirit3000@gmail.com>
2020-06-23 23:05:26 +0300

    feat: life counter

--- src/pages/Savannah/components/GameToolbar/Toolbar.js
+++ src/pages/Savannah/components/GameToolbar/Toolbar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import {withStyles} from "@material-ui/core/styles";
 import {CloseButton} from "../closeButton";
+import {connect} from "react-redux";
 
 const styles = {
   toolBar:{
@@ -16,6 +18,11 @@ const styles = {
   }
 }
 
+function lifeCounterFunction(lifeCounter) {
+  return Array(lifeCounter).fill(1).map(() =>
+    <FavoriteIcon />
+  )
+}
 function GameToolbar(props){
   const { classes } = props;
   return (
@@ -23,9 +30,17 @@ function GameToolbar(props){
       <Typography variant={'h4'} className={classes.title}>
         {props.title}
       </Typography>
+      {props.gameStarted &&
+        lifeCounterFunction(props.lifeCounter)
+      }
       <CloseButton />
     </Toolbar>
   )
 }
 
-export default withStyles(styles)(GameToolbar)
+const mapStateToProps = (store) => {
+  return { lifeCounter: store.savannahReducer.lifeCounter,
+  gameStarted: store.savannahReducer.gameStarted}
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(GameToolbar))
