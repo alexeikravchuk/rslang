09e4de3b4eaa79b2309f416f0df3102015d6ff31
DzianisBogdan <thespirit3000@gmail.com>
2020-06-24 00:00:42 +0300

    feat: reset game

--- src/pages/Savannah/Savannah.js
+++ src/pages/Savannah/Savannah.js
@@ -1,17 +1,11 @@
 import React  from "react";
 import {MainGame} from './components/MainGame'
-import { StartGame } from './components/StartGame'
-import { Statistics } from "./components/Statistics";
 import './Savannah.scss'
-
 
 function Savannah () {
   return (
     <div>
-      <StartGame />
       <MainGame />
-      <Statistics />
-
     </div>
   )
 }
