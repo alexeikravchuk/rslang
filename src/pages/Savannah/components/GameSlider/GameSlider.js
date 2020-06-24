a6ed6ade0643f7ea19084e8218a02eb5044ddf2e
DzianisBogdan <thespirit3000@gmail.com>
2020-06-24 23:50:14 +0300

    feat: game difficulty slider

---
+++ src/pages/Savannah/components/GameSlider/GameSlider.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import {difficultyChange} from "../../../../store/actions/savannahAction";
+import {connect} from "react-redux";
+
+const styles = {
+  root: {
+    width: 300,
+    marginBottom: '2rem',
+  },
+  sliderLabel: {
+    color: "#662246",
+    textAlign: 'center',
+    marginBottom: '2rem'
+  },
+};
+
+function valuetext(value) {
+  return `${value}`;
+}
+
+function GameSlider(props){
+  const {classes} = props;
+    return (
+      <div className={classes.root}>
+        <Typography id="discrete-slider" gutterBottom className={classes.sliderLabel}>
+          Choose your skill:
+        </Typography>
+        <Slider
+          onChangeCommitted={(e, val) => props.onChange(val)}
+          value = {props.difficulty}
+          getAriaValueText={valuetext}
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks
+          min={1}
+          max={6}
+        />
+      </div>
+    );
+}
+
+const mapStateToProps = store => {
+  const { savannahReducer } = store
+  return { ...savannahReducer }
+}
+
+const mapDispatchToProps = dispatch => ({
+  onChange: (sliderValue) => {
+    dispatch(difficultyChange(sliderValue))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(GameSlider))
