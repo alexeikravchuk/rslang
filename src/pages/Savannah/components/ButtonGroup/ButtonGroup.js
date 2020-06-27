1dcfd06670bfdebd397eab5caf2d40fbb96745b4
DzianisBogdan <thespirit3000@gmail.com>
2020-06-27 19:03:06 +0300

    feat: main game button group

--- src/pages/Savannah/components/ButtonGroup/ButtonGroup.js
+++ src/pages/Savannah/components/ButtonGroup/ButtonGroup.js
@@ -8,7 +8,7 @@ import TrainWord from "../TrainWord/TrainWord";
 import {
   addLearnedWords, addMissedWords,
   gameEnding,
-  lifeDecrease, removeWord,
+  lifeDecrease, reload
 } from "../../../../store/actions/savannahAction";
 
 const styles = {
@@ -19,81 +19,27 @@ const styles = {
   },
 }
 
-const defaultState = {
-  showWord: false,
-  animationEnded: true,
-  chosenWord: '',
-}
-
 class GameButtonGroup extends Component {
-  constructor(props) {
-    super(props);
-    this.setEnd = this.setEnd.bind(this)
-    this.setStart = this.setStart.bind(this)
-    this.state = {
-      showWord: false,
-      animationEnded: true,
-      chosenWord: '',
-      trainWord:'',
-    };
-  }
-
-  missAtAnimationEnd(word){
-    this.props.onMiss(this.props.lifeCounter, word);
-  }
-
-  setEnd = () => {
-    if (this.state.chosenWord === '') {this.missAtAnimationEnd(this.state.trainWord)}
-    this.props.onRemove(this.state.trainWord);
-    this.setState(defaultState);
-    setTimeout(() => this.setState({showWord: true}), 200)
-  }
-
-  setStart = () => {
-    this.setState({animationEnded: false, showWord: true, trainWord: this.props.newWords[0].word});
-  }
 
   wordTranslate;
-  checkWord(word){
-    (word === this.state.trainWord) ? this.props.onGuess(word) : this.props.onMiss(this.props.lifeCounter, word);
-  }
-
   onClick(word){
-    this.checkWord(word);
-    this.props.onRemove(this.state.trainWord);
-    this.setState(defaultState);
-    this.timer = setTimeout(() => this.setState({showWord: true}), 200)
+    (word === this.props.newWords[0].word) ? this.props.onGuess(word) : this.props.onMiss(this.props.lifeCounter, word)
   }
 
   componentDidMount() {
-    this.props.fetch(randomInteger(0,30), this.props.difficulty);
-    this.setState({showWord: true})
-  }
-
-  componentWillUnmount() {
-    if (this.timer) {
-      clearTimeout(this.timer)
-    }
+    this.props.fetch(randomInteger(0,30), this.props.difficulty)
   }
 
   render() {
     const { classes } = this.props;
     return (
       <div>
-        {(this.props.newWords.length > 0 && this.state.showWord) &&
-        <TrainWord
-          currentWord={this.props.newWords[0].word}
-          onAnimationStart={this.setStart.bind(this)}
-          onAnimationEnd={this.setEnd.bind(this)}
-        />}
-        {this.state.showWord && <Container className={classes.root}>
-          {shuffleArray(this.props.newWords.slice(0, 4)).map((el, index) => {
-            return <SavannahButton key={el.id}
-                                   title={el.wordTranslate}
-                                   onClick={() => this.onClick(el.word)}
-            />
+        {this.props.newWords.length > 0 && <TrainWord currentWord={this.props.newWords[0].word}/>}
+        <Container className={classes.root}>
+          {this.props.newWords.slice(0, 4).map((el, index) => {
+            return <SavannahButton key={index} title={el.wordTranslate} onClick={()=> this.onClick(el.word)}/>
           })}
-        </Container>}
+        </Container>
       </div>
     )
   }
@@ -112,9 +58,6 @@ const mapDispatchToProps = dispatch => ({
   onGuess: (learnedWord) => {
     dispatch(addLearnedWords(learnedWord))
   },
-  onRemove: (word) => {
-    dispatch(removeWord(word))
-  }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(GameButtonGroup))
