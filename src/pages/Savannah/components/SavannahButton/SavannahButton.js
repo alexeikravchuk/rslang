270f299c9c5e82ae992ebd2fd5ca93450c7f7474
DzianisBogdan <thespirit3000@gmail.com>
2020-06-27 23:36:47 +0300

    refactor: warnings refactor

--- src/pages/Savannah/components/SavannahButton/SavannahButton.js
+++ src/pages/Savannah/components/SavannahButton/SavannahButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import {makeStyles} from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme=> ({
+const useStyles = makeStyles(()=> ({
   button: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     border: 0,
