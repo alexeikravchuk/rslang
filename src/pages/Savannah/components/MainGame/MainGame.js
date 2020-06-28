d363a922120f2c18ecdd67571c36fe385e07e1c6
DzianisBogdan <thespirit3000@gmail.com>
2020-06-28 18:22:40 +0300

    feat: loader

--- src/pages/Savannah/components/MainGame/MainGame.js
+++ src/pages/Savannah/components/MainGame/MainGame.js
@@ -13,6 +13,7 @@ import {
 import {getWords} from "../../utils/wordRequest";
 import {StartGame} from "../StartGame";
 import GameButtonGroup from "../ButtonGroup/ButtonGroup";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const styles ={
   root: {
@@ -22,21 +23,37 @@ const styles ={
     flexDirection: 'column',
     alignContent: 'center',
     justifyContent: 'center'
-
   },
   title: {
     flexGrow: 1,
     textAlign: 'center',
     color: "#662246",
+  },
+  loader: {
+    position: 'relative',
+    zIndex: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height:'80vh',
+    width: '100%'
   }
 }
 
 class MainGame extends Component{
+  loader(classes) {
+    return (
+      <Container className={classes}>
+        <CircularProgress />
+      </Container>
+    )
+  }
   render() {
     const { classes } = this.props;
     return (
       <div>
         <GameToolbar title={''} onClose={this.props.onClose}/>
+        {this.props.loading && this.loader(classes.loader)}
         <Container className={classes.root}>
           {this.props.gameStarted === false && <StartGame />}
           {this.props.gameEnd && <Statistics />}
