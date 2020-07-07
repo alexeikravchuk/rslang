26a7575bbeaa30568aef5844cb99c5094f4628f0
DzianisBogdan <thespirit3000@gmail.com>
2020-07-07 17:08:19 +0300

    refactor: container layout

--- src/pages/Savannah/components/MainGame/Savannah.js
+++ src/pages/Savannah/components/MainGame/Savannah.js
@@ -6,12 +6,13 @@ import {Statistics} from '../Statistics';
 import {getWords} from '../../utils/wordRequest';
 import {StartGame} from '../StartGame';
 import GameButtonGroup from '../ButtonGroup/ButtonGroup';
-import './savannah.scss'
+import './savannah.scss';
 import {
   gameEnding,
   loadWords,
   loadWordsSuccess,
 } from '../../../../store/actions/savannahAction';
+import Grid from '@material-ui/core/Grid';
 
 const styles = {
   title: {
@@ -25,50 +26,63 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    height:'80vh',
-    width: '100%'
-  }
-}
+    height: '80%',
+    width: '100%',
+  },
+};
 
-class Savannah extends Component{
+class Savannah extends Component {
   loader(classes) {
     return (
       <Container className={classes}>
-        <CircularProgress />
+        <CircularProgress/>
       </Container>
-    )
+    );
   }
+
   render() {
-    const { classes, loading, gameStarted, onClose, gameEnd, fetchWords} = this.props;
+    const {classes, loading, gameStarted, onClose, gameEnd, fetchWords} = this.props;
     return (
-      <div  className={'mainGame'}>
+      <div className={'mainGame'}>
         {loading && this.loader(classes.loader)}
-        {(gameStarted && !loading) && <GameToolbar title={''} onClose={onClose} to={'/savannah'}/>}
-        <Container className={'savannahRoot'}>
-          {gameStarted === false && <StartGame />}
-          {gameEnd && <Statistics />}
-          {(gameStarted && !gameEnd) && <GameButtonGroup
-            fetch={fetchWords}
-            visible={true}
-          />}
-        </Container>
+        <Grid
+          container
+          direction={'column'}
+          justify={'space-between'}
+        >
+          <Grid item xs={12}>
+            {(gameStarted && !loading) && <GameToolbar title={''} onClose={onClose} to={'/savannah'}/>}
+          </Grid>
+          <Grid item xs={12}>
+            <Container className={'savannahRoot'}>
+              {gameStarted === false && <StartGame/>}
+              {gameEnd && <Statistics/>}
+              {(gameStarted && !gameEnd) && <GameButtonGroup
+                fetch={fetchWords}
+                visible={true}
+              />}
+            </Container>
+          </Grid>
+        </Grid>
+
+
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps(store){
-  const { savannahReducer } = store
-  return { ...savannahReducer }
+function mapStateToProps(store) {
+  const {savannahReducer} = store;
+  return {...savannahReducer};
 }
 
 const mapDispatchToProps = dispatch => ({
   fetchWords: (page, category) => {
-    dispatch(loadWords())
+    dispatch(loadWords());
     getWords(page, category)
-      .then(json => dispatch(loadWordsSuccess(json)))
+      .then(json => dispatch(loadWordsSuccess(json)));
   },
-  onClose: ()=> dispatch(gameEnding())
-})
+  onClose: () => dispatch(gameEnding()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Savannah));
--- src/pages/Savannah/components/SavannahButton/SavannahButton.js
+++ src/pages/Savannah/components/SavannahButton/SavannahButton.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import {makeStyles, Button} from '@material-ui/core';
 
-const useStyles = makeStyles((theme)=> ({
-  button: {
+const useStyles = makeStyles(()=> ({
+  buttonSavannah: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     border: 0,
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(106, 35, 72, .3)',
     color: 'wheat',
-    height: 48,
+    height: 42,
     width: '100%',
-    margin: theme.spacing(1),
+    padding: '1rem 2rem',
+    overflow: 'hidden'
   },
 }));
 
 export default ({onClick, keyId, disabled, title}) => {
   const classes = useStyles();
   return (
-      <Button key={keyId} variant={'outlined'} className={classes.button}
+      <Button key={keyId} variant={'outlined'} className={classes.buttonSavannah}
         onClick={onClick}
         disabled={disabled}>
         {title}
