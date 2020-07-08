c2dbd0885c00924b13bea063fec77cbd533fbb66
DzianisBogdan <thespirit3000@gmail.com>
2020-07-08 15:59:25 +0300

    style: code reformat

--- src/pages/Savannah/components/ButtonGroup/ButtonGroup.js
+++ src/pages/Savannah/components/ButtonGroup/ButtonGroup.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withStyles, Container, Slide} from '@material-ui/core';
-import {shuffleArray} from '../../utils/utils'
+import {shuffleArray} from '../../utils/utils';
 import {SavannahButton} from '../SavannahButton';
 import TrainWord from '../TrainWord/TrainWord';
 import {
@@ -14,18 +14,18 @@ import Grid from '@material-ui/core/Grid';
 const buttonLimit = 4;
 
 const styles = {
-  buttonGroupRoot:{
+  buttonGroupRoot: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
-}
+};
 
 const defaultState = {
   showWord: false,
   animationEnded: true,
   chosenWord: '',
-}
+};
 
 class GameButtonGroup extends Component {
   constructor(props) {
@@ -34,59 +34,61 @@ class GameButtonGroup extends Component {
       showWord: false,
       animationEnded: true,
       chosenWord: '',
-      trainWord:'',
+      trainWord: '',
     };
   }
 
-  loadNewWords(){
-    if(this.props.newWords.length < 5) {
+  loadNewWords() {
+    if (this.props.newWords.length < 5) {
       this.props.onLevelUp();
       this.props.fetch(this.props.gameLevel, this.props.difficulty);
     }
   }
 
-  missAtAnimationEnd(word){
+  missAtAnimationEnd(word) {
     this.props.onMiss(this.props.lifeCounter, word);
   }
 
   setEnd = () => {
-    if (this.state.chosenWord === '') {this.missAtAnimationEnd(this.state.trainWord)}
+    if (this.state.chosenWord === '') {
+      this.missAtAnimationEnd(this.state.trainWord);
+    }
     this.props.onRemove(this.state.trainWord);
     this.loadNewWords();
     this.setState(defaultState);
-    setTimeout(() => this.setState({showWord: true}), 200)
-  }
+    setTimeout(() => this.setState({showWord: true}), 200);
+  };
 
   setStart = () => {
     this.setState({animationEnded: false, showWord: true, trainWord: this.props.newWords[0]});
-  }
+  };
 
-  checkWord(wordToCheck){
+  checkWord(wordToCheck) {
     const {trainWord} = this.state;
     return (wordToCheck === trainWord.word) ? this.props.onGuess(trainWord) : this.props.onMiss(this.props.lifeCounter, trainWord);
   }
 
-  onClick(word){
+  onClick(word) {
     this.checkWord(word);
     this.props.onRemove(this.state.trainWord);
     this.loadNewWords();
     this.setState(defaultState);
-    this.timer = setTimeout(() => this.setState({showWord: true}), 200)
+    this.timer = setTimeout(() => this.setState({showWord: true}), 200);
   }
 
   componentDidMount() {
-    this.loadNewWords()
-    this.setState({showWord: true})
+    this.loadNewWords();
+    this.setState({showWord: true});
   }
 
   componentWillUnmount() {
     if (this.timer) {
-      clearTimeout(this.timer)
+      clearTimeout(this.timer);
     }
   }
 
   render() {
-    const { classes, newWords, } = this.props;
+    const {classes, newWords} = this.props;
     return (
       <div>
         {(newWords.length > 0 && this.state.showWord) &&
@@ -104,41 +106,41 @@ class GameButtonGroup extends Component {
                   alignItems='center'>
               {shuffleArray(newWords.slice(0, buttonLimit)).map((el, index) => {
                 return (
-                  <Grid item xs={12}  sm={6} md={3} key={index}>
+                  <Grid item xs={12} sm={6} md={3} key={index}>
                     <SavannahButton keyId={el.id}
                                     title={el.wordTranslate}
                                     onClick={() => this.onClick(el.word)}
                     />
                   </Grid>
-                )
+                );
               })}
             </Grid>
-        </Container>
+          </Container>
         </Slide>}
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps(store){
-  const { savannahReducer } = store
-  return { ...savannahReducer }
+function mapStateToProps(store) {
+  const {savannahReducer} = store;
+  return {...savannahReducer};
 }
 
 const mapDispatchToProps = dispatch => ({
   onLevelUp: () => {
-    dispatch(levelUp())
+    dispatch(levelUp());
   },
   onMiss: (lifeCounter, missedWord) => {
     dispatch(addMissedWords(missedWord));
-    (lifeCounter < 1) ? dispatch(gameEnding()) : dispatch(lifeDecrease())
+    (lifeCounter < 1) ? dispatch(gameEnding()) : dispatch(lifeDecrease());
   },
   onGuess: (learnedWord) => {
-    dispatch(addLearnedWords(learnedWord))
+    dispatch(addLearnedWords(learnedWord));
   },
   onRemove: (word) => {
-    dispatch(removeWord(word))
-  }
-})
+    dispatch(removeWord(word));
+  },
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(GameButtonGroup))
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(GameButtonGroup));
--- src/pages/Savannah/components/GameToolbar/Toolbar.js
+++ src/pages/Savannah/components/GameToolbar/Toolbar.js
@@ -6,28 +6,28 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import {CloseButton} from '../closeButton';
 
 const styles = {
-  toolBar:{
+  toolBar: {
     display: 'flex',
     position: 'relative',
     textAlign: 'center',
     color: '#662246',
     zIndex: 10,
     justifyContent: 'space-between',
-    width: '100%'
+    width: '100%',
   },
   title: {
     flexGrow: 1,
-  }
-}
+  },
+};
 
 function lifeCounterFunction(lifeCounter) {
-  return Array(lifeCounter).fill(1).map(( el ,index) =>
-    <FavoriteIcon key={index}/>
-  )
+  return Array(lifeCounter).fill(1).map((el, index) =>
+    <FavoriteIcon key={index}/>,
+  );
 }
 
-function GameToolbar(props){
-  const { classes, title, to, onClose, gameStarted, lifeCounter, gameLevel} = props;
+function GameToolbar(props) {
+  const {classes, title, to, onClose, gameStarted, lifeCounter, gameLevel} = props;
   return (
     <Toolbar className={classes.toolBar}>
       {gameStarted &&
@@ -39,15 +39,15 @@ function GameToolbar(props){
       {gameStarted && <Typography variant={'h6'} className={''}>
         Ваш уровень: {gameLevel}
       </Typography>}
-      <Link to={to} >
+      <Link to={to}>
         <CloseButton onClick={onClose}/>
       </Link>
     </Toolbar>
-  )
+  );
 }
 
-const mapStateToProps = ({ savannahReducer: { lifeCounter, gameStarted, gameLevel } }) => {
-  return { lifeCounter, gameStarted, gameLevel }
-}
+const mapStateToProps = ({savannahReducer: {lifeCounter, gameStarted, gameLevel}}) => {
+  return {lifeCounter, gameStarted, gameLevel};
+};
 
-export default connect(mapStateToProps)(withStyles(styles)(GameToolbar))
+export default connect(mapStateToProps)(withStyles(styles)(GameToolbar));
