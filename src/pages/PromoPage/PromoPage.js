9cff317b10c8e6e1ec442403f09752c564e7ed7e
Vladislav Shevel <vladislav.shevel@gmail.com>
2020-07-08 22:13:30 +0300

    fix: refactoring of main element and styles on page

--- src/pages/PromoPage/PromoPage.js
+++ src/pages/PromoPage/PromoPage.js
@@ -1,36 +1,38 @@
 import React from 'react';
-import Link from '@material-ui/core/Link';
 import {
   Avatar,
+  Grid,
   Typography,
   Container,
   Paper,
+  Link,
   List,
   ListItem,
   ListItemText,
   ListItemIcon
 } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Background from './PromoPageImages/PromoBg.png';
-import { imagesData } from './PromoPageImages/imagesData';
+import firstImage from './PromoPageImages/1.jpg';
+import secondImage from './PromoPageImages/2.jpg';
 import VideogameAssetOutlinedIcon from '@material-ui/icons/VideogameAssetOutlined';
 import MenuBookOutlinedIcon from '@material-ui/icons/MenuBookOutlined';
 import EqualizerOutlinedIcon from '@material-ui/icons/EqualizerOutlined';
 
 const useStyles = makeStyles({
   container: {
-    height: '100vh',
+    width: '100%',
+    height: '100%',
     backgroundImage: "url(" + Background + ")",
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    padding: '20px',
   },
   image: {
     maxWidth: '400px',
-    maxHeight: '300px',
   },
   green: {
-    color: '#32a852',
+    color: '#73ff77',
   },
   blue: {
     color: '#08b1ff',
@@ -41,15 +43,21 @@ const useStyles = makeStyles({
   white: {
     color: '#ffffff',
   },
-  mainTextBlock: {
-    display: 'inline-block',
-    textAlign: 'right',
+  englishEasy: {
+    textAlign: 'center',
   },
-  imagesContainer: {
-    display: 'flex',
-    justifyContent: 'space-around',
-    marginTop: '15px',
-  }
+  playNowLink: {
+    color: '#ff0000',
+    fontSize: '26px',
+    backgroundColor: '#faffe9',
+    borderRadius: '5px',
+    border: '2px solid #73ff77',
+    padding: '5px',
+  },
+  paper: {
+    width: 'min-content',
+    margin: '0 auto',
+  },
 });
 
 export const PromoPage = () => {
@@ -60,73 +68,110 @@ export const PromoPage = () => {
       disableGutters={true}
       className={classes.container}
     >
-      <div className={classes.mainTextBlock}>
-        <Typography
-          color='secondary'
-          variant='h2'
+      <Grid
+        container
+        justify="space-around"
+        alignItems="center"
+        spacing={2}
+      >
+        <Grid
+          item lg={4} md='auto' sm='auto' xs='auto'
         >
-          ENGLISH IS <span className={classes.blue}>EASY</span>
-        </Typography>
-        <Typography
-          color='primary'
-          variant='h6'
+          <div className={classes.englishEasy}>
+            <Typography
+              color='secondary'
+              variant='h2'
+            >
+              ENGLISH IS <span className={classes.blue}>EASY</span>
+            </Typography>
+            <Typography
+              color='primary'
+              variant='h6'
+            >
+              All you need - is a little <strong className={classes.green}>effort!</strong>
+            </Typography>
+            <Typography
+              color='primary'
+              variant='h6'
+            >
+              Let's do it <strong className={classes.orange}>Together!</strong>
+              <p>
+                What are you waiting for?
+            </p>
+              <Link
+                component={RouterLink}
+                to="/choosegame"
+                className={classes.playNowLink}
+              >
+                Play Now
+            </Link>
+            </Typography>
+          </div>
+        </Grid>
+        <Grid
+          item lg={5} md='auto' sm='auto' xs='auto'
         >
-          All you need - is a little <strong className={classes.green}>effort!</strong>
-        </Typography>
-        <Typography
-          color='primary'
-          variant='h6'
-        >
-          Let's do it <strong className={classes.orange}>Together!</strong>
-        </Typography>
-      </div>
-      <div className={classes.featuresBlock}>
-        <Typography
-          color='secondary'
-          variant='h2'
-        >
-          <List>
-            <ListItem>
-              <ListItemIcon>
-                <Avatar>
-                  <VideogameAssetOutlinedIcon className={classes.green} fontSize='large' />
-                </Avatar>
-              </ListItemIcon>
-              <ListItemText primary='GAMES' secondary='Play different games to show your skill' />
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <Avatar>
-                  <MenuBookOutlinedIcon className={classes.white} fontSize='large' />
-                </Avatar>
-              </ListItemIcon>
-              <ListItemText primary='DICTIONARY' secondary='Improve your vocabulary' />
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <Avatar>
-                  <EqualizerOutlinedIcon color='secondary' fontSize='large' />
-                </Avatar>
-              </ListItemIcon>
-              <ListItemText primary='STATS' secondary='Check your progress' />
-            </ListItem>
-          </List>
-        </Typography>
-      </div>
-      <div className={classes.imagesContainer}>
-        {imagesData.map((image) => (
           <Paper
             elevation={2}
+            className={classes.paper}
           >
             <img
-              src={image.img}
-              alt={image.title}
+              src={firstImage}
+              alt='first'
               className={classes.image}
             />
           </Paper>
-        ))}
-      </div>
+        </Grid>
 
+        <Grid
+          item lg={5} md='auto' sm='auto' xs='auto'
+        >
+          <Typography
+            color='secondary'
+          >
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <Avatar>
+                    <VideogameAssetOutlinedIcon className={classes.green} fontSize='large' />
+                  </Avatar>
+                </ListItemIcon>
+                <ListItemText primary='GAMES' secondary='Play different games to show your skill'/>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <Avatar>
+                    <MenuBookOutlinedIcon className={classes.white} fontSize='large' />
+                  </Avatar>
+                </ListItemIcon>
+                <ListItemText primary='DICTIONARY' secondary='Improve your vocabulary' />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <Avatar>
+                    <EqualizerOutlinedIcon color='secondary' fontSize='large' />
+                  </Avatar>
+                </ListItemIcon>
+                <ListItemText primary='STATS' secondary='Check your progress' />
+              </ListItem>
+            </List>
+          </Typography>
+        </Grid>
+        <Grid
+          item lg={5} md='auto' sm='auto' xs='auto'
+        >
+          <Paper
+            elevation={3}
+            className={classes.paper}
+          >
+            <img
+              src={secondImage}
+              alt='second'
+              className={classes.image}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
