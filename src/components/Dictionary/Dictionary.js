303ba2ab4332376b553f8721eaa64ad03711c114
DzianisBogdan <thespirit3000@gmail.com>
2020-07-10 16:53:40 +0300

    feat: List words

--- src/components/Dictionary/Dictionary.js
+++ src/components/Dictionary/Dictionary.js
@@ -2,11 +2,14 @@ import React from 'react';
 // import './Dictionary.css';
 import {URL} from './constants';
 import {getWords} from './getWords.js';
-import Button from '@material-ui/core/Button';
 import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Pagination from '@material-ui/lab/Pagination';
+import {ListItem, ListItemText} from '@material-ui/core';
+import List from '@material-ui/core/List';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 class Dictionary extends React.Component {
 
@@ -17,13 +20,18 @@ class Dictionary extends React.Component {
       content: 'Please select a category',
       category: 0,
       count: 0,
+      pageLoaded: true,
     };
   }
 
-  handleChange(event, value) {
+  handleCategoryChange(event, value) {
     this.setState(() => (
-      {category: value - 1}
+      {category: value - 1, count: 0}
     ), () => this.handleCategory(this.state.category));
+  }
+
+  handleCountChange(event, value) {
+    this.setState(() => ({count: value - 1}), () => this.handleMoreWords());
   }
 
   componentDidMount() {
@@ -35,115 +43,72 @@ class Dictionary extends React.Component {
     document.title = `Results page ${this.state.count}`;
   }
 
-  playAudioWords(audioSrc) {
-    if (!this.audio) {
-      this.audio = new Audio(audioSrc);
-      this.audio.play();
-      this.audio.addEventListener('ended', () => {
-        this.audio = null;
-      });
-      console.log(audioSrc);
+  playAudioWords(path) {
+    let audio = new Audio(path);
+    let audioPromise = audio.play();
+    if (audioPromise !== undefined) {
+      audioPromise.then(_ => {
+        audio.pause();
+      })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 
-  getNewWords = (data, category) => {
-    let words = [];
-    let transcriptions = [];
-    let translations = [];
-    let audios = [];
+  getNextWords = (data) => {
+    let words;
+    let audios;
     data.forEach(item => {
-      words.push(item.word);
-      transcriptions.push(item.transcription);
-      translations.push(item.wordTranslate);
       audios.push(item.audio);
     });
-    words = words.map((item, index) =>
-      <div><span> {item} </span><span
-        className="transcription"> {transcriptions[index]} </span><span> {translations[index]} </span>
-        <Button
-          startIcon={<RecordVoiceOverIcon/>}
-          onClick={() => {
-            this.playAudioWords(`${URL}${audios[index]}`);
-          }}>
-        </Button>
-      </div>);
-    this.setState({
-      content: words,
-      category: category,
-      count: 1,
+    words = data.map((el, index) => {
+      return (
+        <ListItem key={index}>
+          <ListItemText
+            primary={`${index + 1}. ${el.word} - ${el.transcription} - ${el.wordTranslate}`}
+          />
+          <ListItemSecondaryAction>
+            <IconButton edge="end"
+                        onClick={() => {
+                          this.playAudioWords(`${URL}${audios[index]}`);
+                        }}>
+              <RecordVoiceOverIcon/>
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      );
     });
-  };
-
-  getNextWords = (data, counter) => {
-    // const URL = "https://raw.githubusercontent.com/alexeikravchuk/rslang-data/master/";
-    if (this.state.count < 1) {
-      this.setState({
-        count: 0,
-      });
-    } else if (this.state.count < 30) {
-      let words = [];
-      let transcriptions = [];
-      let translations = [];
-      let audios = [];
-      data.forEach(item => {
-        words.push(item.word);
-        transcriptions.push(item.transcription);
-        translations.push(item.wordTranslate);
-        audios.push(item.audio);
-      });
-      words = words.map((item, index) =>
-        <div><span> {item} </span><span
-          className="transcription"> {transcriptions[index]} </span><span> {translations[index]} </span>
-          <Button
-            startIcon={<RecordVoiceOverIcon/>}
-            onClick={() => {
-              this.playAudioWords(`${URL}${audios[index]}`);
-            }}>
-          </Button>
-        </div>);
-      this.setState({
-        content: words,
-        count: counter + 1,
-      });
-    } else {
-      this.setState({
-        count: 30,
-      });
-    }
+    this.setState({content: words});
   };
 
   handleCategory = (categoryNumber) => {
     getWords(0, categoryNumber).then((data) => {
-      this.getNewWords(data, categoryNumber);
+      this.getNextWords(data, categoryNumber);
     });
   };
 
   handleMoreWords = () => {
+    this.setState({pageLoaded: true});
     getWords(this.state.count, this.state.category).then((data) => {
+      this.setState({pageLoaded: true});
       this.getNextWords(data, this.state.count);
     });
   };
 
   render() {
     return (
-      <div className="wrapper">
-        <Container>
-          <Typography variant="h4" component="h2">
-            Dictionary
-          </Typography>
-          <p className="select-category"><i>Category: </i></p>
-          <Pagination count={6} size="medium" onChange={this.handleChange.bind(this)}/>
-          {/*<h2 id="anchor">Essential english words</h2>*/}
-          <div className="dictionaryPage">{this.state.content}</div>
-          <Button variant={'outlined'}
-                  color={'primary'}
-                  onClick={() => this.handleMoreWords()}>
-            Show more word
-          </Button>
-          <p className="page">Results page {this.state.count}</p>
-        </Container>
-
-      </div>
+      <Container>
+        <Typography variant="h4" component="h2">
+          Dictionary
+        </Typography>
+        <p className="select-category"><i>Category: </i></p>
+        <Pagination count={6} size="medium" onChange={this.handleCategoryChange.bind(this)}/>
+        <List dense={true}> {this.state.content}</List>
+        {this.state.pageLoaded && <Pagination count={30}
+                                              size="medium"
+                                              onChange={this.handleCountChange.bind(this)}/>}
+      </Container>
     );
   }
 }
