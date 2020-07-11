6257fd0e0cbd84f15831e040403592aba9b4fe45
DzianisBogdan <thespirit3000@gmail.com>
2020-07-11 17:54:45 +0300

    refactor: animation

--- src/pages/Savannah/components/ButtonGroup/ButtonGroup.js
+++ src/pages/Savannah/components/ButtonGroup/ButtonGroup.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {withStyles, Container, Slide} from '@material-ui/core';
+import {withStyles, Container, Grid, Zoom} from '@material-ui/core';
 import {playFileSound, pubAudioPath, shuffleArray} from '../../utils/utils';
 import {SavannahButton} from '../SavannahButton';
 import TrainWord from '../TrainWord/TrainWord';
@@ -9,7 +9,6 @@ import {
   gameEnding, levelUp,
   lifeDecrease, removeWord,
 } from '../../../../store/actions/savannahAction';
-import Grid from '@material-ui/core/Grid';
 
 const buttonLimit = 4;
 const correct = pubAudioPath('correct');
@@ -17,15 +16,16 @@ const errorSound = pubAudioPath('error');
 const levelUpSound = pubAudioPath('levelUp');
 const styles = {
   groupRoot: {
-    height: '100%'
+    height: '100%',
   },
-  trainWord:{
-    height: '45vh'
+  trainWord: {
+    height: '45vh',
   },
-  buttonGroupContainer:{
+  buttonGroupContainer: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 };
 
@@ -98,46 +98,46 @@ class GameButtonGroup extends Component {
   render() {
     const {classes, newWords} = this.props;
     return (
-        <Grid
-          container
-          direction={'column'}
-          justify="space-between"
-          alignItems="center"
-          className={classes.groupRoot}
-          xs={12}
-        >
-          <Grid item className={classes.trainWord}>
-            {(newWords.length > 0 && this.state.showWord) &&
-            <TrainWord
-              currentWord={newWords[0]}
-              onAnimationStart={this.setStart.bind(this)}
-              onAnimationEnd={this.setEnd.bind(this)}
-            />}
-          </Grid>
-          <Grid item xs={12}>
-            {this.state.showWord &&
-            <Slide direction={'up'} in={this.state.showWord}>
-              <Container className={classes.buttonGroupContainer}>
-                <Grid container
-                      spacing={2}
-                      justify='center'
-                      alignItems='center'>
-                  {shuffleArray(newWords.slice(0, buttonLimit)).map((el, index) => {
-                    return (
-                      <Grid item xs={12} sm={6} md={3} key={index}>
-                        <SavannahButton keyId={el.id}
-                                        title={el.wordTranslate}
-                                        onClick={() => this.onClick(el.word)}
-                        />
-                      </Grid>
-                    )
-                  })}
-                </Grid>
-              </Container>
-            </Slide>}
-          </Grid>
+      <Grid
+        container
+        direction={'column'}
+        justify="space-between"
+        alignItems="center"
+        className={classes.groupRoot}
+        xs={12}
+      >
+        <Grid item className={classes.trainWord}>
+          {(newWords.length > 0 && this.state.showWord) &&
+          <TrainWord
+            currentWord={newWords[0]}
+            onAnimationStart={this.setStart.bind(this)}
+            onAnimationEnd={this.setEnd.bind(this)}
+          />}
         </Grid>
-    )
+        <Grid item xs={12}>
+          {this.state.showWord &&
+          <Zoom in={this.state.showWord}>
+            <Container className={classes.buttonGroupContainer}>
+              <Grid container
+                    spacing={2}
+                    justify='center'
+                    alignItems='center'>
+                {shuffleArray(newWords.slice(0, buttonLimit)).map((el, index) => {
+                  return (
+                    <Grid item xs={12} sm={6} md={3} key={index}>
+                      <SavannahButton keyId={el.id}
+                                      title={el.wordTranslate}
+                                      onClick={() => this.onClick(el.word)}
+                      />
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Container>
+          </Zoom>}
+        </Grid>
+      </Grid>
+    );
   }
 }
 
