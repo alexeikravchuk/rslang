34e548c4f5cd805798b4b93fe8e72a3b775d3a9d
DzianisBogdan <thespirit3000@gmail.com>
2020-07-15 16:33:35 +0300

    feat: fetch info func

--- src/components/Dictionary/getWords.js
+++ src/components/Dictionary/getWords.js
@@ -1,8 +1,14 @@
+import {WORD_INFO_REQUEST_URL} from '../../constants/urlsRequests';
+
 async function getWords(page, category) {
   const dataURL = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${category}`;
   const response = await fetch(dataURL);
-  const data = await response.json();
-  return data;
+  return await response.json();
 }
 
-export { getWords };
+const getWordInfo = (word) => {
+  return fetch(`${WORD_INFO_REQUEST_URL}${word}`)
+    .then(res => res.json());
+};
+
+export {getWords, getWordInfo};
