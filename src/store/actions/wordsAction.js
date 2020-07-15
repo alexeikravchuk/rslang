07ffb1da23e5fc0e7c21a53f7543f5328111890e
DzianisBogdan <thespirit3000@gmail.com>
2020-07-15 16:32:34 +0300

    feat: word info request action

--- src/store/actions/wordsAction.js
+++ src/store/actions/wordsAction.js
@@ -2,31 +2,40 @@ export const REQUEST_WORDS = 'REQUEST-WORDS';
 export const DELETE_WORD = 'DELETE-WORD';
 export const SET_HARD_WORD = 'SET_HARD_WORD';
 export const SET_LEARNED = 'SET_LEARNED';
+export const REQUEST_WORD_INFO_SUCCESS = 'REQUEST_WORD_INFO_SUCCESS';
+export const REQUEST_WORD_INFO = 'REQUEST_WORD_INFO';
 
-// async fetch words from server
-export const loadWords = (requestWords) => {
+export const requestWordInfo = () => {
   return ({
-    type: REQUEST_WORDS,
-    requestWords,
-  })
+    type: REQUEST_WORD_INFO,
+  });
+};
+
+export const requestWordInfoSuccess = (word) => {
+  return (
+    {
+      type: REQUEST_WORD_INFO_SUCCESS,
+      word,
+    }
+  );
 };
 
 export const deleteWord = (deleteFunc) => {
   return ({
     type: DELETE_WORD,
     deleteFunc,
-  })
+  });
 };
 
 export const setHardWord = (setHard) => {
   return ({
     type: SET_HARD_WORD,
     setHard,
-  })
-}
+  });
+};
 
 export const setLearned = () => {
   return ({
     type: SET_LEARNED,
-  })
-}
+  });
+};
