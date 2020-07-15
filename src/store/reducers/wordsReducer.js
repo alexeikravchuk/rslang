b3728ba541b58f0fa70566aa775ff14b0e83c639
DzianisBogdan <thespirit3000@gmail.com>
2020-07-15 16:29:11 +0300

    feat: word info request

--- src/store/reducers/wordsReducer.js
+++ src/store/reducers/wordsReducer.js
@@ -1,22 +1,33 @@
-import { SET_HARD_WORD, DELETE_WORD, REQUEST_WORDS, SET_LEARNED} from '../actions'
+import {SET_HARD_WORD, DELETE_WORD, SET_LEARNED} from '../actions';
+import {REQUEST_WORD_INFO, REQUEST_WORD_INFO_SUCCESS} from '../actions/wordsAction';
 
 const defaultState = {
-  hardWords: [],
-  learnedWords: [],
-  deletedWords: [],
-  newWords: [],
-}
+    hardWords: [{word: 'agree', id: '5e9f5ee35eb9e72bc21af4a1'}, {word: 'alcohol', id: '5e9f5ee35eb9e72bc21af4a0'},
+      {word: 'agree', id: '5e9f5ee35eb9e72bc21af4a1'}, {word: 'alcohol', id: '5e9f5ee35eb9e72bc21af4a0'}, {
+        word: 'agree',
+        id: '5e9f5ee35eb9e72bc21af4a1',
+      }, {word: 'alcohol', id: '5e9f5ee35eb9e72bc21af4a0'}],
+    learnedWords: [{word: 'agree', id: '5e9f5ee35eb9e72bc21af4a1'}, {word: 'alcohol', id: '5e9f5ee35eb9e72bc21af4a0'}],
+    deletedWords: [{word: 'agree', id: '5e9f5ee35eb9e72bc21af4a1'}, {word: 'alcohol', id: '5e9f5ee35eb9e72bc21af4a0'}],
+    newWords: [],
+    currentWord: '',
+    loadingWord: false,
+  }
+;
 
-const wordReducer = ( state = defaultState, action) => {
+const wordReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_HARD_WORD: {
       return {...state};
     }
-    case DELETE_WORD:{
+    case DELETE_WORD: {
       return {...state};
     }
-    case REQUEST_WORDS: {
-      return {...state};
+    case REQUEST_WORD_INFO: {
+      return {...state, loadingWord: true};
+    }
+    case REQUEST_WORD_INFO_SUCCESS: {
+      return {...state, currentWord: action.word, loadingWord: false};
     }
     case SET_LEARNED: {
       return {...state};
@@ -25,6 +36,6 @@ const wordReducer = ( state = defaultState, action) => {
       return {...state};
     }
   }
-}
+};
 
 export default wordReducer;
