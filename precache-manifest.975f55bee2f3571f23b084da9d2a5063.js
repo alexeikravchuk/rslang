e92d3c9a8084ec78cb3970c74fdc1a22ba8af397
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-07-17 13:22:50 +0300

    fix: word build

---
+++ precache-manifest.975f55bee2f3571f23b084da9d2a5063.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc417b8f62cb9a21c55825a2388ff1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03ac4246e6f50c8bf351",
+    "url": "/static/css/main.83411c8f.chunk.css"
+  },
+  {
+    "revision": "7e827ada010559afbaec",
+    "url": "/static/js/2.0d1f7432.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.0d1f7432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ac4246e6f50c8bf351",
+    "url": "/static/js/main.58f75273.chunk.js"
+  },
+  {
+    "revision": "d929cf65ec2b90611e40",
+    "url": "/static/js/runtime-main.1e2373d1.js"
+  },
+  {
+    "revision": "a33bde23a3d37f0b3a980f0640d00fb1",
+    "url": "/static/media/2.a33bde23.jpg"
+  },
+  {
+    "revision": "03e8edda018ad9d7bfa317e90233966f",
+    "url": "/static/media/Spin-0.7s-144px.03e8edda.svg"
+  },
+  {
+    "revision": "5e75b57b8b28205d6c5941ed5f12d30a",
+    "url": "/static/media/audio.5e75b57b.svg"
+  },
+  {
+    "revision": "275d8fc86a398ef7a0b91c263ef4d1ac",
+    "url": "/static/media/audiocall-background.275d8fc8.jpg"
+  },
+  {
+    "revision": "e232982160cacf902865fd43be079ea8",
+    "url": "/static/media/background.e2329821.jpg"
+  },
+  {
+    "revision": "23afba556b7876408fd4321310506876",
+    "url": "/static/media/bg.23afba55.png"
+  },
+  {
+    "revision": "e0628d65ee953792649ee8754e83a7b4",
+    "url": "/static/media/cardground.e0628d65.JPG"
+  },
+  {
+    "revision": "70454ee3aa3306bce8d1d8df83d4304a",
+    "url": "/static/media/correct.70454ee3.mp3"
+  },
+  {
+    "revision": "e90d216902135470dc92937e3b209778",
+    "url": "/static/media/default-audiocall.e90d2169.png"
+  },
+  {
+    "revision": "08374cfe2c727f4639e8f3765c914af1",
+    "url": "/static/media/dictionaryBack.08374cfe.jpg"
+  },
+  {
+    "revision": "b67dc6634135f3717d57b38314405392",
+    "url": "/static/media/disabled.b67dc663.jpg"
+  },
+  {
+    "revision": "32fc22b2ceef21006a7f31e18e44f1f3",
+    "url": "/static/media/error.32fc22b2.mp3"
+  },
+  {
+    "revision": "79c66246545ce5712c1408a208137fb5",
+    "url": "/static/media/game-background.79c66246.jpg"
+  },
+  {
+    "revision": "8883f86733cef54e5d0219031859186a",
+    "url": "/static/media/homepagebg.8883f867.png"
+  },
+  {
+    "revision": "9d6952da5fb54668a59bf0f8965b9c29",
+    "url": "/static/media/logo.9d6952da.png"
+  },
+  {
+    "revision": "a9ed6cc51d3304884bcdb5bdcdeb1642",
+    "url": "/static/media/microphone.a9ed6cc5.svg"
+  },
+  {
+    "revision": "3c6fb25bf2a0e145b302f81d61e51fef",
+    "url": "/static/media/no_avatar.3c6fb25b.png"
+  },
+  {
+    "revision": "5a84b4e194ee8f9901bb8f3c8defdd06",
+    "url": "/static/media/original.5a84b4e1.png"
+  },
+  {
+    "revision": "7e8d3b883d2267c4375e99d0b3f71bc8",
+    "url": "/static/media/speak_it_start_img.7e8d3b88.jpg"
+  },
+  {
+    "revision": "4cf1669951b4a89d31b43d73c0200bc9",
+    "url": "/static/media/star.4cf16699.svg"
+  },
+  {
+    "revision": "c7cf1e65c55eac3a87f7adc8ac4c3cd1",
+    "url": "/static/media/statistics.c7cf1e65.jpg"
+  }
+]);
